77e56d4632343bc87625ebb7fbdd366048e43296
zabudskajuliia <zabudska.juliia@gmail.com>
2016-09-08 22:19:52 +0300

    Update ColorOfDays.java

--- src/com/company/ColorOfDays.java
+++ src/com/company/ColorOfDays.java
@@ -1,20 +1,28 @@
 package com.company;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class ColorOfDays {
     private static String colorForCurrentDay = "\033[36m";
     private static String colorForWeekend = "\033[31m";
     private static String colorForWorkDays = "\033[37m";
 
-    public static String getColorForCurrentDay() {
-        return colorForCurrentDay;
+    private static boolean isWeekend(DayOfWeek getDayOfMonth) {
+        return (getDayOfMonth == DayOfWeek.SATURDAY || getDayOfMonth == DayOfWeek.SUNDAY);
     }
 
-    public static String getColorForWeekend() {
-        return colorForWeekend;
-    }
-
-    public static String getColorForWorkDays() {
-        return colorForWorkDays;
+    public static String colorForDayInWeek(int numberOfDay, LocalDate date) {
+        DayOfWeek getDayOfMonth = DayOfWeek.from(date.withDayOfMonth(numberOfDay));
+        if (date.getDayOfMonth() == numberOfDay) {
+            return colorForCurrentDay + numberOfDay;
+        } else {
+            if (isWeekend(getDayOfMonth)) {
+                return colorForWeekend + numberOfDay;
+            } else {
+                return colorForWorkDays + numberOfDay;
+            }
+        }
     }
 }
 
